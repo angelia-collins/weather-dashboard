b7fb9942e111af6127cb60c875010b72a8a86069
angelia collins <angiecollins09@gmail.com>
2020-11-04 16:14:45 -0700

    fixed scope issue

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-var cityInput = $("input").val().trim();
-var APIKey = "9f613adfc2ebdd9a02e12a81537c05d4";
-
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey;
-
-
-
 $("button").on("click", function (event) {
     event.preventDefault();
+    var cityInput = $("#city").val().trim();
+    var APIKey = "9f613adfc2ebdd9a02e12a81537c05d4";
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey;
 console.log(cityInput);
 
 $.ajax({
