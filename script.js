1281231e88720edcada20bbdbe9eae3efb0ad725
angelia collins <angiecollins09@gmail.com>
2020-11-04 18:39:24 -0700

    display city name

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $("button").on("click", function (event) {
     var cityInput = $("#city").val().trim();
     var APIKey = "9f613adfc2ebdd9a02e12a81537c05d4";
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + APIKey;
 console.log(cityInput);
 
 $.ajax({
@@ -11,6 +11,12 @@ $.ajax({
     method: "GET"
   }).then(function(response) {
     console.log(response);
+
+    var cardBody = $("<div>").addClass("card-body");
+    var cityName = $("<h5>").addClass("card-title").text(response.city.name);
+    
+    cardBody.append(cityName);
+    $(".today").append(cardBody);
 })
 
 });
