cd3d6d824be40a4c085c640e883eee735f10dcac
angelia collins <angiecollins09@gmail.com>
2020-11-07 16:25:34 -0700

    added UV Index and Date

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 $("button").on("click", function (event) {
     event.preventDefault();
     var cityInput = $("#city").val().trim();
@@ -6,17 +7,58 @@ $("button").on("click", function (event) {
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + APIKey;
 console.log(cityInput);
 
+var queryURLToday = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey;
+console.log(cityInput);
+
+// 5 day forecast
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
 
+
+})
+
+// today
+$.ajax({
+    url: queryURLToday,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+    // UV Index
+    var queryURLUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + APIKey;
+
+    $.ajax({
+      url: queryURLUV,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      
+      var UVIndex = $("<p>").addClass("card-text").text("UV Index: " + response.value);
+      var todayDate = $("<h5>").text((response.date_iso).split("T", 1));
+
+      cardBody.append(UVIndex);      
+      cityName.append(todayDate);
+  
+  })
     var cardBody = $("<div>").addClass("card-body");
-    var cityName = $("<h5>").addClass("card-title").text(response.city.name);
+    var cityName = $("<h5>").addClass("card-title").text(response.name);
+    var fToday = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(1);
+    var tempToday = $("<p>").addClass("card-text").text("Temperature: " + fToday);
+    var humidityToday = $("<p>").addClass("card-text").text("Humidity: " + response.main.humidity + "%");
+    var windyToday = $("<p>").addClass("card-text").text("Wind Speed: " + ((response.wind.speed * 2.2369)).toFixed(1) + " MPH");
+
+    var iconcode = response.weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    $('#wicon').attr('src', iconurl);
     
-    cardBody.append(cityName);
+    $(".today").addClass("card");
+    cardBody.append(cityName, tempToday, humidityToday, windyToday);
     $(".today").append(cardBody);
+
+
 })
 
 });
